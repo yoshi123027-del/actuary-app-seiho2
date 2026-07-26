aa2473c58f075b3c22653b140ea5274f449bdea0
yoshi123027-del <yoshi123027@gmail.com>
2026-07-27 07:01:18 +0900

    Add vercel/app/config.js

---
+++ vercel/app/config.js
@@ -0,0 +1,18 @@
+export const appConfig = {
+  subject: "生保2",
+  examDate: "2026-12-09T00:00:00+09:00",
+  storageKey: "actuary_app_seiho2_vercel_v1",
+  menus: ["ホーム", "今日の課題", "章ごとに学ぶ", "問題検索", "教科書で学ぶ", "所見で学ぶ"],
+  textbookLinks: {
+    "1": ["第1章 営業保険料の簡易まとめ", "https://drive.google.com/file/d/13fG1wuq-5YG37DpKszP0RoHpdc4B4P2X/view?usp=drive_link"],
+    "2": ["第2章 解約返戻金の簡易まとめ", "https://drive.google.com/file/d/1olNfAVmUFRDZELWnL2J2-5gmTG5TFPto/view?usp=sharing"],
+    "3": ["第3章 アセットシェアの簡易まとめ", "https://drive.google.com/file/d/1wLd2ihpHmq_AMCoD6TSpQXyUbpGvT0Jw/view?usp=sharing"],
+    "4": ["第4章 商品開発の簡易まとめ", "https://drive.google.com/file/d/1I6IVO8s9DcMpkMIQqAjzqem7lwGhNzkh/view?usp=sharing"],
+    "5": ["第5章 変額年金保険の簡易まとめ", "https://drive.google.com/file/d/1MjpsBUqMcZhWIryYQlyBtaDdOpeCeAE8/view?usp=sharing"],
+    "6": ["第6章 団体生命保険の簡易まとめ", "https://drive.google.com/file/d/1cSjjyWocIR1D44zSNdM5RGeQ_0ZNMjiN/view?usp=sharing"],
+    "7": ["第7章 医療保険の簡易まとめ", "https://drive.google.com/file/d/1nvfrcmni93MDk_18CcRfOurN_wBgS28d/view?usp=sharing"],
+    "8": ["第8章（資料準備中）", ""],
+    "9": ["第9章 再保険の簡易まとめ", "https://drive.google.com/file/d/1HvaoS8TPe7UDnSmAm8ndZg14fcpeBxdT/view?usp=sharing"],
+    "10": ["第10章 商品毎収益検証の簡易まとめ", "https://drive.google.com/file/d/1VEwR7glqF3MBKKXV_eM0ypbdG9wx81SM/view?usp=sharing"]
+  }
+};
